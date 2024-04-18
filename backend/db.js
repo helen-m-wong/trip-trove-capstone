083d3297fc32f852639e5427276853120da800e2
helen-m-wong <helen.mar.wong@gmail.com>
2024-04-17 23:54:33 -0700

    citation

--- backend/db.js
+++ backend/db.js
@@ -1,3 +1,14 @@
+/*
+Based on information from the following articles
+Title: How to Use MongoDB and Mongoose with Node.js
+Author: Stanley Ulili
+URL: https://blog.appsignal.com/2023/08/09/how-to-use-mongodb-and-mongoose-for-nodejs.html
+
+Title: Connect MongoDB database to Express server — step-by-step
+Author: Sharmila S.
+URL: https://medium.com/featurepreneur/connect-mongodb-database-to-express-server-step-by-step-53e548bb4967
+*/
+
 import mongoose from "mongoose";
 
 export default function connectDB() {
