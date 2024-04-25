3e24704798c0111c052c9416fac26714241cfe29
helen-m-wong <helen.mar.wong@gmail.com>
2024-04-24 21:15:23 -0700

    trip schema and create a trip endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,19 @@
 import connectDB from "./db.js";
 import express from "express";
+import bodyParser from 'body-parser'; 
+import routerTrips from "./router.js"
 
 const app = express();
-const port = 8010;
+const port = 3000;
 
 connectDB();
+app.use(bodyParser.json());
+app.use('/trips', routerTrips);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://flip2.engr.oregonstate.edu:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
