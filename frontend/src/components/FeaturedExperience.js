b27f74a0f63b3cf1427d1f323a30f3e7bd1ebe09
Joshua Palces <96431285+jpalces78@users.noreply.github.com>
2024-06-05 14:24:06 -0500

    Google cloud additions
    Added new build folder
    Changed trips and experiences routing on both frontend and backend

--- frontend/src/components/FeaturedExperience.js
+++ frontend/src/components/FeaturedExperience.js
@@ -7,7 +7,7 @@ const FeatureExperience = () => {
     const [currentIndex, setCurrentIndex] = useState(0);
 
     useEffect(() => {
-        fetch('http://localhost:3000/experiences')
+        fetch('/api/experiences')
             .then(response => response.json())
             .then(data => setExperiences(data))
             .catch(error => console.error('Error fetching experience data:', error));
